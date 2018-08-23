fd88a87e3c9d510af9ee71c83fab0e9483c39ec5
James Dowell <james.s.dowell@gmail.com>
2018-08-23 15:56:58 +0000

    added cookie-parser, routing framework

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
+const cookieParser = require("cookie-parser");
 const sass        = require("node-sass-middleware");
 const app         = express();
 
@@ -34,6 +35,7 @@ app.use("/styles", sass({
   outputStyle: 'expanded'
 }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
@@ -42,6 +44,53 @@ app.use("/api/users", usersRoutes(knex));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// Admin route
+app.get('/menu_admin', (req, res) => {
+  res.cookie('cookieName', 'admin');
+  res.redirect('/menu');
+})
+
+// Customer route
+app.get('/menu_customer', (req, res) => {
+  res.cookie('cookieName', 'user');
+  res.redirect('/menu');
+})
+
+// GET - Menu page 
+app.get('/menu', (req, res) => {
+  console.log(req.cookies);
+  if (req.cookies.cookieName = 'admin') {
+    res.render('menu_admin');
+  } else {
+    res.render('menu');
+  }
+})
+
+// PUT - Update inventory
+app.put('/menu')
+
+// GET - View order history
+app.get('/orders', (req, res) => {
+  console.log(req.cookies);
+  if (req.cookies.cookieName = 'admin') {
+    res.render('orders_admin');
+  } else {
+    res.render('orders');
+  }
+})
+
+// POST - Create order   
+app.post('/orders')
+
+// PUT - Owner updates order status
+app.put('/orders/:id')
+
+// GET - User reviews order before submitting
+app.get('/confirmation')
+
+// Add item to cart
+app.put('/cart')
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
